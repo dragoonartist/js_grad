99c6b73769387fc913d13fff1816e8285783d286
Dragoon Artist <dragoon.artist@gmail.com>
2022-03-20 10:31:46 +0900

    login using localstrage

--- js/clock.js
+++ js/clock.js
@@ -6,7 +6,7 @@ const getTime = () => {
   const minutes = String(nowTime.getMinutes()).padStart(2, 0);
   const seconds = String(nowTime.getSeconds()).padStart(2, 0);
 
-  timeText.innerText = `${hours}. ${minutes}. ${seconds}.`;
+  timeText.innerText = `${hours}ㅅ ${minutes}ㅂ ${seconds}ㅊ`;
 };
 
 getTime();
---
+++ js/greetings.js
@@ -0,0 +1,28 @@
+const greetingContainer = document.querySelector(".greetings");
+const greeting = greetingContainer.querySelector("h1");
+
+const greetingForm = greetingContainer.querySelector("#greeting-form");
+const nameInput = greetingForm.querySelector("#username");
+
+const USERNAME_KEY = "username";
+
+function paintGreetings(name) {
+  greetingForm.classList.add("hidden");
+  greeting.innerText = `Hello! ${name}`;
+  greeting.classList.remove("hidden");
+}
+
+const onSubmitName = (event) => {
+  event.preventDefault();
+  const username = nameInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+};
+
+const savedName = localStorage.getItem(USERNAME_KEY);
+
+if (savedName !== null) {
+  paintGreetings(savedName);
+} else {
+  greetingForm.addEventListener("submit", onSubmitName);
+}
